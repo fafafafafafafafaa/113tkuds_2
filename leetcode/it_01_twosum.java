d3bd688b0bcf51c4806c8aa006e2199cd96127f8
fafafafafafafafaa <vivi930319@gmail.com>
2025-09-01 10:07:16 +0800

    add

---
+++ leetcode/it_01_twosum.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class it_01_twosum {
+
+    // twoSum 方法
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> seen = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (seen.containsKey(complement)) {
+                return new int[]{seen.get(complement), i};
+            }
+            seen.put(nums[i], i);
+        }
+        return new int[]{}; // 題目保證有解，理論上不會到這裡
+    }
+
+    // main 測試
+    public static void main(String[] args) {
+        it_01_twosum solution = new it_01_twosum();
+
+        int[] nums1 = {2, 7, 11, 15};
+        int target1 = 9;
+        int[] result1 = solution.twoSum(nums1, target1);
+        System.out.println("Example 1: [" + result1[0] + ", " + result1[1] + "]");
+
+        int[] nums2 = {3, 2, 4};
+        int target2 = 6;
+        int[] result2 = solution.twoSum(nums2, target2);
+        System.out.println("Example 2: [" + result2[0] + ", " + result2[1] + "]");
+
+        int[] nums3 = {3, 3};
+        int target3 = 6;
+        int[] result3 = solution.twoSum(nums3, target3);
+        System.out.println("Example 3: [" + result3[0] + ", " + result3[1] + "]");
+    }
+}
---
+++ leetcode/it_02_addtwonumber.java
@@ -0,0 +1,5 @@
+package leetcode;
+
+public class it_02_addtwonumber {
+    
+}
